b1cc49959f5b235a2d97a7a4963c394137aa6c58
Larry Boltovskoi <larry.bolt@gmail.com>
2015-11-03 00:40:53 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require('./vendor/autoload.php');
+
+$app = new \Slim\App;
+
+// Define app routes
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+$app->get('/', function ($request, $response, $args) {
+    return $response->write("HI");
+});
+
+// Run app
+$app->run();
+
