24c3eaa0e1e43f48c80c71ea3f1a6f9611405ad3
Larry Boltovskoi <larry.bolt@gmail.com>
2015-11-03 01:08:10 +0100

    Added basic auth

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 require('./vendor/autoload.php');
 
+$is_dev = $_SERVER['HTTP_HOST'] == 'composertest.dev';
+
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name', function ($name) {
+$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    "secure" => !$is_dev, // disallow http when in production
+    "users" => [
+        "root" => "t00r",
+        "user" => "passw0rd"
+    ],
+    "callback" => function ($arguments) use ($app) {
+        $app->user = $arguments['user'];
+    }
+]));
+
+$app->get('/hello/:name', function ($name) use ($app) {
     echo "Hello, $name";
+    echo "<br>you are logged in as {$app->user}";
 });
-$app->get('/', function () {
-    echo "HI";
+$app->get('/', function () use($app) {
+    echo "HI {$app->user}";
 });
 
 $app->run();
-
